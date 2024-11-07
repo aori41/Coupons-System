ba4ff5b3fdc96405c8623533cc1f256245151db2
aori41 <aori444@gmail.com>
2024-11-07 23:42:27 +0200

    Added Routing System

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
+import { Route } from "wouter";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { Panel } from "./pages/Panel";
+import { Reports } from "./pages/Reports";
+
 function App() {
 
   return <>
+    <Route path="/" component={Home} />
+    <Route path="/login" component={Login} />
+    <Route path="/panel" component={Panel} />
+    <Route path="/reports" component={Reports} />
   </>
 }
 
---
+++ src/pages/Panel.tsx
@@ -0,0 +1,3 @@
+export const Panel: React.FC = () => {
+	return <></>
+}
