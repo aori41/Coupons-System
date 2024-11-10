8d8f7ca703e27431ae11ba8e839e4293223df29d
aori41 <aori444@gmail.com>
2024-11-10 21:55:07 +0200

    Fixed Modals

---
+++ src/components/modals/CouponModal.tsx
@@ -0,0 +1,208 @@
+import { useState } from "react";
+import { CouponData } from "../../context/Context";
+import { Modal } from "./types";
+import { toast } from "react-toastify";
+import { CustomModal } from "./CustomModal";
+import { Button, Input, RadioGroup, Textarea } from "@nextui-org/react";
+import { RefreshCcw } from "lucide-react";
+import { ColoredRadio } from "../ColoredRadio";
+import { couponController } from "../../controllers/coupon";
+
+const formatDateForInput = (timestamp: number) => {
+	const date = new Date(timestamp);
+
+	const year = date.getFullYear();
+	const month = `${date.getMonth() + 1}`.padStart(2, "0");
+	const day = `${date.getDate()}`.padStart(2, "0");
+	const hours = `${date.getHours()}`.padStart(2, "0");
+	const minutes = `${date.getMinutes()}`.padStart(2, "0");
+
+	return `${year}-${month}-${day}T${hours}:${minutes}`;
+};
+
+export const CouponModal: React.FC<CouponData & Modal> = ({ code, canCombine, discountType, discountAmount, description, limitUses, expirationDate, setLoading }) => {
+	const [couponData, setCouponData] = useState<CouponData>({
+		code,
+		description,
+		discountType,
+		discountAmount,
+		expirationDate,
+		canCombine,
+		limitUses,
+	});
+
+	const resetValues = () => {
+		setCouponData({
+			code,
+			description,
+			discountType,
+			discountAmount,
+			expirationDate,
+			canCombine,
+			limitUses,
+		})
+	}
+
+	const handleCreateCoupon = async () => {
+		if (!couponData.code || !couponData.discountAmount) {
+			toast.error("Failed: Code and Discount amount are required");
+			return false;
+		}
+		setLoading(true);
+
+		const res = await couponController.create(couponData);
+
+		setLoading(false);
+
+		if (res?.message) {
+			toast.error("Failed: " + res.message);
+			return false;
+		}
+		toast.success("Saved the coupon successfully");
+		return true;
+	}
+
+	const generateCouponCode = () => {
+		const code = Math.random().toString(36).substring(2, 8).toUpperCase();
+		setCouponData({ ...couponData, code });
+	};
+
+	return <>
+		<CustomModal
+			buttonText="Create Coupon"
+			title="Create Coupon"
+			onSave={handleCreateCoupon}
+			resetParams={resetValues}
+		>
+			<div className="p-4 w-full flex flex-wrap space-y-4">
+				<div className="w-full md:w-1/2 space-y-4 pr-2 flex flex-col justify-center">
+					<section className="flex flex-col gap-y-8 mt-4">
+						<label htmlFor="coupon-code" className='text-gray-500'>Coupon Code</label>
+						<div className="w-full flex gap-x-3 items-center">
+							<Input
+								id="coupon-code"
+								name="code"
+								isClearable
+								label="Coupon Code"
+								fullWidth
+								classNames={{ clearButton: "text-black" }}
+								value={couponData.code}
+								onChange={(e) => setCouponData({ ...couponData, code: e.target.value })}
+								onClear={() => setCouponData({ ...couponData, code: "" })}
+								aria-describedby="generate-code-desc"
+							/>
+							<Button
+								isIconOnly
+								onClick={generateCouponCode}
+								aria-label="Generate coupon code"
+								id="generate-code-desc"
+							>
+								<RefreshCcw />
+							</Button>
+						</div>
+					</section>
+					<fieldset>
+						<legend className="sr-only">Discount Type</legend>
+						<RadioGroup
+							label="Discount Type"
+							value={couponData.discountType}
+							orientation="horizontal"
+							onChange={(e) => setCouponData({ ...couponData, discountType: e.target.value as "percent" | "ils" })}
+						>
+							<ColoredRadio value="percent" label="Percent" />
+							<ColoredRadio value="ils" label="ILS" />
+						</RadioGroup>
+					</fieldset>
+
+					<label htmlFor="discount-amount" className="sr-only">Discount Amount</label>
+					<Input
+						id="discount-amount"
+						type="number"
+						label="Discount Amount"
+						fullWidth
+						min={1}
+						value={`${couponData.discountAmount}`}
+						onChange={(e) => setCouponData({ ...couponData, discountAmount: +e.target.value })}
+						aria-labelledby="discount-type-amount"
+					/>
+				</div>
+				<div className="w-full md:w-1/2 space-y-4 pl-2 flex flex-col justify-center">
+					<fieldset>
+						<legend className="sr-only">Expiration Date</legend>
+						<RadioGroup
+							label="Expiration Date"
+							value={couponData.expirationDate === undefined ? "never" : "set"}
+							orientation="horizontal"
+							onChange={(e) => {
+								setCouponData({
+									...couponData,
+									expirationDate: (e.target.value === "never" ? undefined : Date.now()),
+								});
+							}}
+						>
+							<ColoredRadio value="never" label="Never Expire" />
+							<ColoredRadio value="set" label="Set Expiration" />
+						</RadioGroup>
+					</fieldset>
+					<label htmlFor="expiration-date" className="sr-only">Expiration Date</label>
+					<Input
+						id="expiration-date"
+						type="datetime-local"
+						value={formatDateForInput(couponData.expirationDate || Date.now())}
+						isDisabled={!couponData.expirationDate}
+						onChange={(e) => setCouponData({ ...couponData, expirationDate: new Date(e.target.value).getTime() })}
+						aria-describedby="expiration-date-desc"
+					/>
+					<fieldset>
+						<legend className="sr-only">Usage Limit</legend>
+						<RadioGroup
+							label="Usage Limit"
+							value={couponData.limitUses <= "0" && couponData.limitUses !== "" ? "unlimited" : "limited"}
+							orientation="horizontal"
+							onChange={(e) => {
+								setCouponData({ ...couponData, limitUses: e.target.value === "unlimited" ? "0" : "1" });
+							}}
+						>
+							<ColoredRadio value="unlimited" label="No Limit" />
+							<ColoredRadio value="limited" label="Set Limit" />
+						</RadioGroup>
+					</fieldset>
+					<label htmlFor="limit-uses" className="sr-only">Limit Uses</label>
+					<Input
+						id="limit-uses"
+						type="number"
+						label="Limit Uses"
+						fullWidth
+						isDisabled={couponData.limitUses <= "0" && couponData.limitUses !== ""}
+						min={1}
+						value={couponData.limitUses}
+						onChange={(e) => setCouponData({ ...couponData, limitUses: e.target.value })}
+					/>
+				</div>
+				<div className="w-full space-y-4">
+					<fieldset>
+						<legend className="sr-only">Can Combine with Other Coupons?</legend>
+						<RadioGroup
+							label="Can Combine with Other Coupons?"
+							value={couponData.canCombine ? "yes" : "no"}
+							onChange={(e) => setCouponData({ ...couponData, canCombine: e.target.value === "yes" })}
+							className="mt-2"
+						>
+							<ColoredRadio value="no" label="Cannot Combine" />
+							<ColoredRadio value="yes" label="Can Combine with Other Coupons" />
+						</RadioGroup>
+					</fieldset>
+					<label htmlFor="description" className="sr-only">Description</label>
+					<Textarea
+						id="description"
+						name="description"
+						label="Description"
+						fullWidth
+						value={couponData.description}
+						onChange={(e) => setCouponData({ ...couponData, description: e.target.value })}
+					/>
+				</div>
+			</div>
+		</CustomModal>
+	</>
+}
--- src/components/modals/CustomModal.tsx
+++ src/components/modals/CustomModal.tsx
@@ -1,44 +1,45 @@
 import { ReactNode, useContext } from "react";
 import { Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, useDisclosure } from "@nextui-org/react";
 import { Save } from "lucide-react";
-import Context from "../context/Context";
+import Context from "../../context/Context";
 
 type ModalProps = {
 	title: string;
 	children: ReactNode;
 	buttonText: string;
-	onSave?: () => boolean;
-	onCancel?: () => void;
+	onSave?: () => Promise<boolean>;
+	resetParams?: () => void;
 }
 
-export const CustomModal: React.FC<ModalProps> = ({ title, buttonText, onSave, onCancel, children }) => {
+export const CustomModal: React.FC<ModalProps> = ({ title, buttonText, onSave, resetParams, children }) => {
 	const { theme } = useContext(Context);
 	const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
 	return <>
 		<Button onPress={onOpen}>{buttonText}</Button>
-		<Modal isOpen={isOpen} onOpenChange={onOpenChange} onClose={onCancel} isDismissable={false} isKeyboardDismissDisabled={true} className={`${theme === "dark" && "bg-[#111827] text-white"} min-w-max`}>
+		<Modal isOpen={isOpen} onOpenChange={onOpenChange} onClose={resetParams} isDismissable={false} isKeyboardDismissDisabled={true} className={`${theme === "dark" && "bg-[#111827] text-white"} min-w-max`}>
 			<ModalContent>
 				{(onClose) => (
 					<>
 						<ModalHeader className="flex flex-col">{title}</ModalHeader>
-						<ModalBody>
+						<ModalBody className="min-h-max min-w-max">
 							{children}
 						</ModalBody>
 						<ModalFooter>
 							<div className="w-full flex justify-between">
 								<Button color="danger" variant="light" onClick={() => {
 									onClose();
-									onCancel && onCancel();
+									resetParams && resetParams();
 								}}>
 									Close
 								</Button>
 								{onSave && <Button
-									onClick={() => {
-										const save = onSave();
+									onClick={async () => {
+										const save = await onSave();
+
 										if (save) {
 											onClose();
-											onCancel && onCancel();
+											resetParams && resetParams();
 										}
 									}}
 									className={`text-white font-bold ${theme === "dark" ? "bg-blue-600 hover:bg-blue-500" : "bg-blue-500 hover:bg-blue-400"}`}
@@ -51,6 +52,6 @@ export const CustomModal: React.FC<ModalProps> = ({ title, buttonText, onSave, o
 					</>
 				)}
 			</ModalContent>
-		</Modal>
+		</Modal >
 	</>
 };
